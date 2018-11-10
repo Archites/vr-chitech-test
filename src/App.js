7b015ad26a791b875e9b67a59b8b5047bd85ee09
babestvl <babestvl@gmail.com>
2018-11-10 15:34:54 +0700

    Update src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 const App = () => (
 	<div className="App">
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-		</header>
+		<header className="App-header">HELP</header>
 	</div>
 )
 
---
+++ src/pages/ExamplePage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExamplePage = () => (
+  <div>
+    Example
+  </div>
+)
+
+export default ExamplePage
